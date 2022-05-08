7dbc4881fedb150977fcc0d0ab3472c20c6cd400
shimu21ph <shimu21ph@gmail.com>
2022-05-08 14:16:38 +0600

    my item added

--- src/Components/Pages/Blogs/Blogs.js
+++ src/Components/Pages/Blogs/Blogs.js
@@ -1,17 +1,63 @@
 import React from 'react';
-import './Blogs.css';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>This is blogs</h2>
-            <div>
-                h1
-            </div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
+        <Container>
+            <Row className="my-4">
+                <Col sm={12} md={6}>
+                    <Card>
+                        <Card.Header as="h5">Featured</Card.Header>
+                        <Card.Body>
+                            <Card.Title>Special title treatment</Card.Title>
+                            <Card.Text>
+                                With supporting text below as a natural lead-in to additional content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col sm={12} md={6}>
+                    <Card>
+                        <Card.Header as="h5">Featured</Card.Header>
+                        <Card.Body>
+                            <Card.Title>Special title treatment</Card.Title>
+                            <Card.Text>
+                                With supporting text below as a natural lead-in to additional content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+
+            <Row className="my-4">
+                <Col sm={12} md={6}>
+                    <Card>
+                        <Card.Header as="h5">Featured</Card.Header>
+                        <Card.Body>
+                            <Card.Title>Special title treatment</Card.Title>
+                            <Card.Text>
+                                With supporting text below as a natural lead-in to additional content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col sm={12} md={6}>
+                    <Card>
+                        <Card.Header as="h5">Featured</Card.Header>
+                        <Card.Body>
+                            <Card.Title>Special title treatment</Card.Title>
+                            <Card.Text>
+                                With supporting text below as a natural lead-in to additional content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
--- src/Components/Pages/MyItem/MyItem.js
+++ src/Components/Pages/MyItem/MyItem.js
@@ -1,10 +1,26 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase/firebase.init';
+import axios from 'axios';
 
 const MyItems = () => {
+    const [user] = useAuthState(auth);
+    const [myItems, setMyItems] = useState([]);
+
+    useEffect(() => {
+        const getServices = async () => {
+            const email = user.email;
+            const url = `http://localhost:5000/myItem?email=${email}`;
+            console.log(url);
+            const { data } = await axios.get(url);
+            setMyItems(data);
+        }
+        getServices();
+    }, [user]);
+
     return (
         <div style={{ height: "90vh" }}>
-            <h2>this is all added my items</h2>
+            <h2>this is all added my items {myItems.length}</h2>
         </div>
     );
 };
